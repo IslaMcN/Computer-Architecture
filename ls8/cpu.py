9ad42cba2d754a49b8b47c798ab872190c4407fb
Isla <fallenacademy@gmail.com>
2020-03-24 12:22:36 -0700

    Working on my interprets

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -36,24 +36,59 @@ class CPU:
     def load(self):
         """Load a program into memory."""
 
-        address = 0
+        try:
+            address = 0
+            with open(sys.argv[1]) as f:
+                for line in f:
+                    comment_split = line.strip().split('#')
+                    value = comment_split[0].strip()
+                    self.ram[address] = int(value, 2)
+                    address += 1
+            print(self.ram)
+        except FileNotFoundError:
+            print('File not Found')
+            sys.exit(2)
 
-        # For now, we've just hardcoded a program:
+    def interpret_PRN(self):
+        self.ram_read(self.ram[self.pc + 1])
+        self.pc += 2
+    
+    def interpret_ADD(self):
+        self.alu("ADD", self.ram[self.pc + 1], self.ram[self.pc + 2])
+        self.pc += 3
+    
+    def interpret_MUL(self):
+        self.alu('MUL', self.ram[self.pc + 1], self.ram[self.pc + 2])
+        self.pc += 3
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+    def interpret_PUSH(self):
+        val = self.register[self.ram[self.pc + 1]]
+        
+    def interpret_POP(self):
+        pass
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+    def interpret_CALL(self):
+        val = self.pc + 2
 
+    def interpret_RET(self):
+        pass
+
+    def interpret_CMP(self):
+        pass 
+
+    def interpret_JMP(self):
+        self.pc = self.register[self.ram[self.pc + 1]]
+    
+    def interpret_JEQ(self):
+        pass
+
+    def interpret_JNE(self):
+        pass
+
+    def interpret_PRA(self):
+        print(self.register[self.ram[self.pc + 1 ]])
+        print('ASCII', chr(self.register[self.ram[self.pc + 1]]))
+        self.pc += 2
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
